6ff654fcb8ded6c0793be91014f92b7738689f07
arun1252 <65217145+arun1252@users.noreply.github.com>
2020-06-02 13:47:16 +0530

    Add files via upload

---
+++ cloudmigration/src/main/java/com/sample/cloudmigration/CloudmigrationApplication.java
@@ -0,0 +1,13 @@
+package com.sample.cloudmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudmigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudmigrationApplication.class, args);
+	}
+
+}
---
+++ cloudmigration/src/test/java/com/sample/cloudmigration/CloudmigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.cloudmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudmigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
